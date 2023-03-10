d346f5961a8c6dfb29d8a9ebfbae5d46b5142606
S7b0t4 <toni.paun.00@mail.ru>
2023-03-10 21:26:42 +0500

    !Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {useRef, useState} from "react";
 
 function App() {
+  const [message, setMessage] = useState()
+  const [user, setUser] = useState({name:"IDK"})
+  const new_user = useRef()
+
+  const sendNewUser = () =>{
+    if(user){
+      fetch("http://localhost:8000/post_api",{
+        method: "POST",
+        body: JSON.stringify(user)
+      }).then(r => console.log(r))
+    }
+  }
+  const getUser = () =>{
+    fetch("http://localhost:8000/api")
+      .then(async res => await res.json())
+      .then((data) =>{
+        setMessage(data.name);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{message}</p>
+      <input type="text" ref={new_user}/>
+      <button onClick={getUser}>+</button>
+      <button onClick={sendNewUser}>+=</button>
     </div>
   );
 }
